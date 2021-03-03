b9753133f3ac1ff06e271cb7d7b0293054afd3d1
Alex McDonald <acm4219@gmail.com>
2021-03-02 21:38:27 -0600

    got a lot of the ground work set for the routes and set the pages

--- roofing-business/client/src/App.js
+++ roofing-business/client/src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+import Navbar from "./components/Navbar";
+import About from "./views/About";
+import Backend from "./views/Backend";
+import Entry from "./views/Entry";
+import FormPage from "./views/FormPage";
+import LeadPage from "./views/LeadPage";
+import Products from "./views/Products";
 
 function App() {
   return (
     <Router>
-      
+      <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Entry />
+          </Route>
+          <Route>
+            <About />
+          </Route>
+          <Route>
+            <Backend />
+          </Route>
+          <Route>
+            <FormPage />
+          </Route>
+          <Route>
+            <LeadPage />
+          </Route>
+          <Route>
+            <Products />
+          </Route>
+        </Switch>
     </Router>
   );
 }
---
+++ roofing-business/client/src/views/LeadPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LeadPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LeadPage;
