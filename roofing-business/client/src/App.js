d7933bcf20c2ae8b7dfcbf854034c9b41144e861
Alex McDonald <acm4219@gmail.com>
2021-03-29 22:48:39 -0500

    trying to sort out how to do heroku bear with me please

--- roofing-business/client/src/App.js
+++ roofing-business/client/src/App.js
@@ -1,39 +1,53 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
-import Navbar from "./components/Navbar";
-import About from "./views/About";
-import Backend from "./views/Backend";
-import Entry from "./views/Entry";
-import FormPage from "./views/FormPage";
-import LeadPage from "./views/LeadPage";
-import Products from "./views/Products";
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container, AppBar, Typography, Grow } from "@material-ui/core";
+import { useDispatch } from 'react-redux'
+
+import {getPosts} from "./actions/posts";
+import Posts from "./views/Request";
+import Form from "./views/Form";
+import Entry from "./views/Entry/Entry";
+import loon from "./images/loon.png";
+import useStyles from "./styles"
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Entry />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/backend">
-            <Backend />
-          </Route>
-          <Route path="/form">
-            <FormPage />
-          </Route>
-          <Route path="/leads">
-            <LeadPage />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
-    </Router>
-  );
+    const [currentId, setCurrentId] = useState(null)    
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
+    return (
+        <Router>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+            <img className={classes.image} src={loon} alt="memories" align="center" height="100" />
+                <Typography className={classes.heading} variant="h2" align="center" component={Link} to="/">Loon Roofing</Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Switch>
+                        <Route exact path="/">
+                            <Entry />
+                        </Route>
+                        <Route path="/requestform">
+                            <Form />
+                        </Route>
+                        <Route path="/requests">
+                            <Posts />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Grow>
+        </Container>
+        <br />
+        </Router>
+
+        
+    )
 }
 
-export default App;
+export default App
