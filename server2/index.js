f5e9d0fbb8109529670dcedfd35d22f5fd9e8c1d
Alex McDonald <acm4219@gmail.com>
2021-04-11 19:07:52 -0500

    trying a different method of connection

--- server2/index.js
+++ server2/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb://LoonAdmin1:Ks9XJ5MA8C@acm4219-loonroofing-mongo-production/loonroofing';
+const CONNECTION_URL = 'mongodb://LoonAdmin1:Ks9XJ5MA8C@acm4219-loonroofing-mongo-production/admin';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
